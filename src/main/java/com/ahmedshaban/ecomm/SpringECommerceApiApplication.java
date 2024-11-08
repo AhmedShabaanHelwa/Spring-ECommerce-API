aecdac479f7143b1daf3fc0f6d826ea309fa9d93
Ahmed Shaban <ahmed.shaban.ext@siemens.com>
2024-11-08 23:17:40 +0200

    Initial Spring project.

---
+++ src/main/java/com/ahmedshaban/ecomm/SpringECommerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.ahmedshaban.ecomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringECommerceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringECommerceApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ahmedshaban/ecomm/SpringECommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmedshaban.ecomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringECommerceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
