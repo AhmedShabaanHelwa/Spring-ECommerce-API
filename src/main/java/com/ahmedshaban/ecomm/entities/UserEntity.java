5c83b7bc90851661765e29952ac8187513871161
Ahmed Shaban <ahmed.shaban.ext@siemens.com>
2024-11-09 07:44:48 +0200

    Add UserEntity.

---
+++ src/main/java/com/ahmedshaban/ecomm/entities/UserEntity.java
@@ -0,0 +1,101 @@
+package com.ahmedshaban.ecomm.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "user")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id", nullable = false, updatable = false)
+    private UUID id;
+
+    @NotNull(message = "Username is required.")
+    @Basic(optional = false)
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    @Basic(optional = false)
+    private String password;
+
+    @Column(name = "first_name")
+    @Basic(optional = false)
+    private String firstName;
+
+    @Column(name = "last_name")
+    @Basic(optional = false)
+    private String lastName;
+
+    @Column(name = "email")
+    @Basic(optional = false)
+    private String email;
+
+    @Column(name = "phone")
+    @Basic(optional = false)
+    private String phone;
+
+    @Column(name = "user_status")
+    @Basic(optional = false)
+    private String userStatus;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(String userStatus) {
+        this.userStatus = userStatus;
+    }
+}
