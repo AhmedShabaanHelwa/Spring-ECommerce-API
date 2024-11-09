12a3484bf03eb63ad7877758be483b388991d21e
Ahmed Shaban <ahmed.shaban.ext@siemens.com>
2024-11-09 07:43:39 +0200

    Create migration script.

---
+++ src/main/resources/db/migration/V1.0.0__Init.sql
@@ -0,0 +1,39 @@
+CREATE SCHEMA IF NOT EXISTS ecomm;
+
+CREATE TABLE IF NOT EXISTS ecomm."user" (
+    id              uuid NOT NULL DEFAULT random_uuid(),
+    username        varchar(16),
+    password        varchar(40),
+    first_name      varchar(16),
+    last_name       varchar(16),
+    email           varchar(24),
+    phone           varchar(24),
+    user_status     varchar(16),
+    PRIMARY KEY(id)
+);
+
+create TABLE IF NOT EXISTS ecomm.address (
+    id              uuid NOT NULL DEFAULT random_uuid(),
+    number          varchar(24),
+    residency       varchar(32),
+    street          varchar(32),
+    city            varchar(24),
+    state           varchar(24),
+    country         varchar(24),
+    pincode         varchar(10),
+    PRIMARY KEY(id)
+);
+
+create TABLE IF NOT EXISTS ecomm.user_address (
+    user_id         uuid NOT NULL DEFAULT random_uuid(),
+    address_id      uuid NOT NULL,
+    FOREIGN KEY (user_id)
+        REFERENCES ecomm."user"(id),
+    FOREIGN KEY(address_id)
+        REFERENCES ecomm.address(id)
+);
+
+insert into ecomm."user" (id, username, password, first_name, last_name, email, phone, user_status) values('a1b9b31d-e73c-4112-af7c-b68530f38222', 'test', 'pwd', 'Test', 'User', 'test@user.com', '234234234', 'ACTIVE');
+insert into ecomm."user" (id, username, password, first_name, last_name, email, phone, user_status) values('a1b9b31d-e73c-4112-af7c-b68530f38223', 'test', 'pwd', 'Test2', 'User2', 'test2@user.com', '234234234', 'ACTIVE');
+INSERT INTO ecomm.address VALUES ('a731fda1-aaad-42ea-bdbc-a27eeebe2cc0', '9I-999', 'Fraser Suites Le Claridge', 'Champs-Elysees', 'Paris', 'Île-de-France', 'France', '75008');
+insert into ecomm.user_address values ('a1b9b31d-e73c-4112-af7c-b68530f38222', 'a731fda1-aaad-42ea-bdbc-a27eeebe2cc0');
