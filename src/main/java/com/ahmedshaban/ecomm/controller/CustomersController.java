b8c374fe18ae34df336fe00c17917e9a383ffd6e
Ahmed Shaban <ahmed.shaban.ext@siemens.com>
2024-11-10 19:56:00 +0200

    Introduce getCustomerById in CustomersController.

--- src/main/java/com/ahmedshaban/ecomm/controller/CustomersController.java
+++ src/main/java/com/ahmedshaban/ecomm/controller/CustomersController.java
@@ -4,8 +4,12 @@ import com.ahmedshaban.ecomm.entities.CustomerEntity;
 import com.ahmedshaban.ecomm.service.CustomerService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
+import static org.springframework.http.ResponseEntity.notFound;
 import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
@@ -23,5 +27,11 @@ public class CustomersController{
     public ResponseEntity<Iterable<CustomerEntity>> getAllCustomers() {
 
         return ok(customerService.getAllCustomers());
+    }
+
+    @GetMapping(value = "/api/v1/customers/{id}")
+    public ResponseEntity<CustomerEntity> getCustomerById(@PathVariable("id") UUID id) {
+
+        return customerService.getCustomerById(id).map(ResponseEntity::ok).orElse(notFound().build());
     }
 }
