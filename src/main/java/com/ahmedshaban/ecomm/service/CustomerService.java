9a03c034cdd198190a66a5179c9e268997b62275
Ahmed Shaban <ahmed.shaban.ext@siemens.com>
2024-11-10 19:55:23 +0200

    Add more methods to CustomerService to cover more HTTP verbs.

--- src/main/java/com/ahmedshaban/ecomm/service/CustomerService.java
+++ src/main/java/com/ahmedshaban/ecomm/service/CustomerService.java
@@ -1,8 +1,21 @@
 package com.ahmedshaban.ecomm.service;
 
+import com.ahmedshaban.ecomm.entities.AddressEntity;
 import com.ahmedshaban.ecomm.entities.CustomerEntity;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public interface CustomerService {
 
     Iterable<CustomerEntity> getAllCustomers();
+
+    Optional<CustomerEntity> getCustomerById(UUID id);
+
+    void deleteCustomerById(String id);
+
+    void deleteCustomer(CustomerEntity customerEntity);
+
+    Optional<Iterable<AddressEntity>> getAddressesByCustomerId(String id);
+
 }
--- src/main/java/com/ahmedshaban/ecomm/service/CustomerServiceImpl.java
+++ src/main/java/com/ahmedshaban/ecomm/service/CustomerServiceImpl.java
@@ -1,8 +1,12 @@
 package com.ahmedshaban.ecomm.service;
 
+import com.ahmedshaban.ecomm.entities.AddressEntity;
 import com.ahmedshaban.ecomm.entities.CustomerEntity;
 import com.ahmedshaban.ecomm.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
@@ -20,4 +24,29 @@ public class CustomerServiceImpl implements CustomerService {
 
         return customerRepository.findAll();
     }
+
+    @Override
+    public Optional<CustomerEntity> getCustomerById(UUID id) {
+
+        return customerRepository.findById(id);
+    }
+
+    @Override
+    public void deleteCustomerById(String id) {
+
+        customerRepository.deleteById(UUID.fromString(id));
+    }
+
+    @Override
+    public void deleteCustomer(CustomerEntity customerEntity) {
+
+        customerRepository.delete(customerEntity);
+    }
+
+    @Override
+    public Optional<Iterable<AddressEntity>> getAddressesByCustomerId(String id) {
+
+        return customerRepository.findById(UUID.fromString(id)).map(CustomerEntity::getAddresses);
+    }
+
 }
